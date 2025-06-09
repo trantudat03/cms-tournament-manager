785760a19f4f8de5fc9eeb182a0781ab08468b4c
trantudat03 <trantudat03@gmail.com>
2025-06-09 14:54:37 +0700

    update cloudinary

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,21 @@
-export default () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "cloudinary",
+      providerOptions: {
+        cloud_name: env("CLOUDINARY_NAME", "dtfrotdax"),
+        api_key: env("CLOUDINARY_KEY", "188385232158984"),
+        api_secret: env("CLOUDINARY_SECRET"),
+      },
+      actionOptions: {
+        upload: {},
+        delete: {},
+      },
+    },
+  },
+  "users-permissions": {
+    config: {
+      jwtSecret: env("JWT_SECRET", "yourDefaultSecretHere"),
+    },
+  },
+});
