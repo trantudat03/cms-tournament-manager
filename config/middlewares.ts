99e496620b4347b9e2721f56b6d4f4357ba6f638
trantudat03 <trantudat03@gmail.com>
2025-06-09 15:21:32 +0700

    update cloudinary

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,43 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          "media-src": ["'self'", "data:", "blob:", "res.cloudinary.com"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: "strapi::cors",
+    config: {
+      enabled: true,
+      origin: "*", // Chấp nhận tất cả domain
+      headers: "*",
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Cho phép tất cả phương thức HTTP
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  {
+    name: "strapi::body",
+    config: {
+      formLimit: "256mb", // modify form body
+      jsonLimit: "256mb", // modify JSON body
+      textLimit: "256mb", // modify text body
+      formidable: {
+        maxFileSize: 200 * 1024 * 1024, // multipart data, modify here limit of uploaded file size
+      },
+    },
+  },
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
